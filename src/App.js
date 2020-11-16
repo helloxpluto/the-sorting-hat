56faf3d5afa7ba568b0523618d5f51a816cad291
helloxpluto <deplanetart@gmail.com>
2020-11-15 18:33:39 -0800

    adding url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Link, Route, Redirect } from 'react-router-dom';
+import Body from './Body'
+import Header from './Header'
 import './App.css';
 
 function App() {
+  const [house, setHouse] = useState('')
+  const [houseInfo, setHouseInfo] = useState('')
+  const [houseImage, setHouseImage] = useState('')
+  
+  const url = 
   return (
     <div className="App">
+      <Body/>
+      <Header/>
     
     </div>
   );
--- src/Body.js
+++ src/Body.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Body = () => {
     return (
         <div>
-            
+            <h1>hello</h1>
         </div>
     );
 };
