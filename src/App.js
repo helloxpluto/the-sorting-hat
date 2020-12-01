cbe0f5ddc351a31a4ea725285570fa30bf3fc108
helloxpluto <deplanetart@gmail.com>
2020-11-30 20:55:04 -0800

    have all items functioning on page

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { Link, Route, Redirect } from 'react-router-dom';
 import Body from './components/Body';
 import Header from './components/Header';
-import { Card, Button, Form } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import './App.css';
-import Houses from './components/Houses';
 import Hat from './components/Hat';
 
 function App() {
-	// const [house, setHouse] = useState('');
-	const [houseInfo, setHouseInfo] = useState('');
-//   const [userName, setUserName] = useState('name');
 
-	// useEffect(() => {
-	// 	const url = 'https://www.potterapi.com/v1/sortingHat';
-	// 	fetch(url)
-	// 		.then((res) => res.json())
-	// 		.then((res) => setHouse(res))
-	// 		.catch(console.error);
-	// }, []);
-
-	// function handleSubmit(event) {
-	// 	event.preventDefault();
-
-	// 	var useName = userName 
-	
-	// }
-
-	// function handleChange (event) {
-	// 	event.preventDefault()
-	// 	setUserName(
-	// 		event.target.input
-	// 	)
-	// 	console.log(setUserName)
-		
-	// }
 
 	return (
 		<div className='App'>
-			<Header />
+			<Header className='title' />
 			<Card className='textCenter'>
 				<Card.Header>
 					<div className='hat'>
@@ -46,45 +18,8 @@ function App() {
 					</div>
 				</Card.Header>
 				<Body />
-				{/* <Card.Body className='cardBody'>
-					<Card.Text>
-						Enter your name and let the sorting hat choose your HOUSE
-					</Card.Text>
-					<Body house={house} />
-					<Form.Group controlId="formPlaintextName" className='formCard'>
-						<Form.Control
-							className='nameForm'
-							size='lg'
-							type='text'
-							placeholder='Enter your name'
-						
-						/>
-							<Button className='findButton' variant='outline-warning' type='submit' onClick={handleChange}>
-							Find my house
-						</Button>
-					</Form.Group>
-				</Card.Body> */}
 			</Card>
-			{console.log(Form)}
-			{/* <form onSubmit={handleSubmit}>
-				<input
-					type='text'
-					placeholder='Enter your name'
-					onChange={(event) => {
-						setUserName({
-							name: event.target.input,
-						});
-						{
-							console.log(event.input);
-						}
-					}}
-				/>
-				<>
-					<Button className='findButton' variant='outline-danger'>Find my house</Button>{' '}
-					
-					
-				</>
-			</form> */}
+		
 		</div>
 	);
 }
--- src/components/Body.js
+++ src/components/Body.js
@@ -2,12 +2,28 @@ import React, { useState, useEffect } from 'react';
 import { Form, Button, Card } from 'react-bootstrap';
 
 
+
+
 const Body = () => {
 	const [house, setHouse] = useState('');
 
 	const [userName, setUserName] = useState('');
 
 	const [displayName, setDisplayName] = useState('')
+
+	const [houseName, setHouseName] = useState('')
+
+	const [houseChange, setHouseChange] = useState('')
+	
+	const Gryffindor = 'Gryffindor values bravery, daring, nerve, and chivalry.Its emblematic animal is the lion, and its colours are scarlet and gold. Minerva McGonagall was the most recent Head of Gryffindor. Sir Nicholas de Mimsy-Porpington, also known as "Nearly Headless Nick", was the House Ghost. The founder of the House was Godric Gryffindor. Gryffindor corresponds to the element of Fire. The common room was located in one of the highest towers at Hogwarts, the entrance was situated on the seventh floor in the east wing of the castle and is guarded by a portrait of The Fat Lady. She permits entrance if given the correct password, which is changed numerous times throughout the school year. Famous Gryffindors include Albus Dumbledore, Harry Potter, and Minerva McGonagall.'
+
+	const Hufflepuff = `Hufflepuff values hard work, dedication, patience, loyalty, and fair play. Its emblematic animal is the badger, and yellow and black are its colours. Pomona Sprout was the Head of Hufflepuff during 1991-1998, Sprout left the post of Head of Hufflepuff and Herbology Professor sometime before 2017 and her successor for the position of Head of Hufflepuff is currently unknown. The Fat Friar is its ghost. The founder of the House was Helga Hufflepuff.`
+
+	const Ravenclaw = `Ravenclaw values intelligence, knowledge, curiosity, creativity and wit. Its emblematic animal is the eagle, and its colours are blue and bronze. The Ravenclaw Head of House in the 1990s was Filius Flitwick. The ghost of Ravenclaw is the Grey Lady, who was the daughter of Rowena Ravenclaw, the House's founder. Ravenclaw corresponds to the element of air. The Ravenclaw common room and dormitories are located in a tower on the west side of the castle. Ravenclaw students must answer a riddle as opposed to giving a password to enter their dormitories. This riddle, however, can be answered by non-Ravenclaws. Famous Ravenclaws include Luna Lovegood, Gilderoy Lockhart, Ignatia Wildsmith (inventor of Floo powder), and Garrick Ollivander.`
+
+	const Slytherin = `Slytherin House values ambition, leadership, self-preservation, cunning and resourcefulness and was founded by Salazar Slytherin. Its emblematic animal is the serpent, and its colours are emerald green and silver. Professor Horace Slughorn was the Head of Slytherin during the 1997–1998 school year, replacing Severus Snape, who as well, replaced Slughorn as Potions Professor when he retired for the first time several years prior. Slytherin had produced the most Death Eaters and Dark Wizards, including Tom Riddle, Bellatrix Lestrange and Lucius Malfoy, for example. But that does not mean that other Houses haven’t produced any; Peter Pettigrew was a Gryffindor, and Quirinus Quirrell was a Ravenclaw.
+        The Bloody Baron is the House Ghost. The founder of the House was Salazar Slytherin.`
+
 
 	useEffect(() => {
 		const url = 'https://www.potterapi.com/v1/sortingHat';
@@ -16,7 +32,20 @@ const Body = () => {
 			.then((res) => setHouse(res))
 			.catch(console.error);
 	}, []);
+	console.log(house);
 
+	function changeHouses() {
+		if (house === 'Gryffindor') {
+			setHouseChange(Gryffindor)
+		} else if (house === 'Hufflepuff') {
+			setHouseChange(Hufflepuff)
+		} else if (house === 'Ravenclaw') {
+			setHouseChange(Ravenclaw)
+		} else if (house === 'Slytherin') {
+			setHouseChange(Slytherin)
+		}
+	}
+	
 
 
 	
@@ -31,35 +60,61 @@ const Body = () => {
 		
 		
 	}
+
+	function handleHouseName(event) {
+		event.preventDefault()
+		changeHouses();
+		
+		
+			setHouseName(houseChange);
+		
+	}
+	
 	
 	return (
 		<div>
-			{displayName && `${displayName} house is ${house}`}
 			<Card className='textCenter'>
 				<Card.Body className='cardBody'>
+					{displayName && `${displayName}'s house is ${house}`}
+
 					<Card.Text>
+						<br />
 						Enter your name and let the sorting hat choose your HOUSE
 					</Card.Text>
-				<Form onSubmit={handleSubmit}>
-
-					<Form.Group  controlId='name' className='formCard'>
-						<Form.Control
-							className='nameForm'
-							size='lg'
-							type='text'
-							placeholder='Enter your name'
-							onChange={handleChange}
-							value={userName}
-						/>
-					</Form.Group>
+					<Form onSubmit={handleSubmit}>
+						<Form.Group controlId='name' className='formCard'>
+							<Form.Control
+								className='nameForm'
+								size='lg'
+								type='text'
+								placeholder='Enter your name'
+								onChange={handleChange}
+								value={userName}
+							/>
+						</Form.Group>
 						<Button
 							className='findButton'
 							variant='outline-warning'
-
 							type='submit'>
 							Find my house
 						</Button>
-				</Form>
+					</Form>
+					{displayName && (
+						<Button
+							className='houseButton'
+							variant='outline-warning'
+							type='submit'
+							onClick={handleHouseName}>
+							Show my house information
+						</Button>
+					)}
+					{displayName && houseName && (
+						<Card>
+							<Card.Body className='bodyText'>
+								<div>{houseName}</div>
+							</Card.Body>
+						</Card>
+					)}
 				</Card.Body>
 			</Card>
 		</div>
